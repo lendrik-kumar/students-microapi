054568ebad5e96391b9817d2841296f0352bd740
lendrik-kumar <sanketkamboj2006@gmail.com>
2025-11-19 21:42:36 +0530

    first post route setuped

--- cmd/students-api/main.go
+++ cmd/students-api/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/lendrik-kumar/students-microapi/internal/config"
+	"github.com/lendrik-kumar/students-microapi/internal/http/handlers/student"
 )
 
 func main() {
@@ -13,24 +20,39 @@ func main() {
 
 	router := http.NewServeMux()
 
-	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world!"))
-	})
+	router.HandleFunc("POST /api/students", student.New())
 
-	router.HandleFunc("POST /hello", func(w http.ResponseWriter, r *http.Request) {
-
-	})
-
+	slog.Info("server is starting ", slog.String("address", cfg.HttpServer.Addr))
 	fmt.Printf("server listining on %s", cfg.HttpServer.Addr)
 	server := http.Server{
 		Addr:    cfg.HttpServer.Addr,
 		Handler: router,
 	}
 
-	err := server.ListenAndServe()
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("failed to start server")
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err := server.Shutdown(ctx)
 
 	if err != nil {
-		log.Fatal("failed to start server")
+		slog.Error("failed to shutdown", slog.String("error", err.Error()))
 	}
+
+	slog.Info("server shutdown sucessfully")
 
 }
---
+++ internal/http/handlers/student/student.go
@@ -0,0 +1,42 @@
+package student
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/lendrik-kumar/students-microapi/internal/types"
+	"github.com/lendrik-kumar/students-microapi/internal/utils/response"
+)
+
+func New() http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("creating a student")
+
+		var student types.Student
+
+		err := json.NewDecoder(r.Body).Decode(&student)
+		if errors.Is(err, io.EOF) {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(fmt.Errorf("empty Body")))
+			return
+		}
+
+		if err != nil {
+			response.WriteJson(w, http.StatusBadRequest, response.GeneralError(err))
+			return 
+		}
+
+		//validator 
+		if err := validator.New().Struct(student); err != nil {
+			validateError := err.(validator.ValidationErrors)
+			response.WriteJson(w, http.StatusBadRequest, response.ValidationError(validateError))
+			return
+		}
+
+		response.WriteJson(w, http.StatusCreated, map[string]string{"success": "OK"})
+	}
+}
